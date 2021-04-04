d4580b217108c332c58745bac7dcd9bb555d7843
Enrique Coello <enrique.coello@gmail.com>
2021-04-04 03:20:52 -0400

    Added a new unsorted string list class

---
+++ src/main/java/unsorted/UnsortedStringList.java
@@ -0,0 +1,41 @@
+package unsorted;
+
+public class UnsortedStringList {
+
+    int maxItems;
+
+    public void UnsortedStringList(int maxItems) {
+    }
+
+    public void UnsortedStringList() {
+
+    }
+
+    public boolean isFull() {
+        return true;
+    }
+
+    public int lenghtIs() {
+        return 0;
+    }
+
+    public boolean isThere(String item) {
+        return true;
+    }
+
+    public void insert(String item) {
+
+    }
+
+    public void delete(String item) {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public String getNextItem() {
+        return new String();
+    }
+}
