a4a6c51c00f0e030f6941d7298fa62c699ce19e5
Enrique Coello <enrique.coello@gmail.com>
2021-04-04 03:39:22 -0400

    Updated all methods in new list class

--- src/main/java/unsorted/UnsortedStringList.java
+++ src/main/java/unsorted/UnsortedStringList.java
@@ -1,41 +1,76 @@
 package unsorted;
 
 public class UnsortedStringList {
-
-    int maxItems;
+    // The number of items increases as items are added to the array
+    protected int numItems;
+    protected String[] list;
+    protected int currentPos;
+    protected int maxItems;
 
     public void UnsortedStringList(int maxItems) {
+        numItems = 0;
+        list = new String[maxItems];
     }
 
     public void UnsortedStringList() {
-
+        numItems = 0;
+        list = new String[100];
     }
 
     public boolean isFull() {
-        return true;
+        return (list.length == numItems);
     }
 
     public int lenghtIs() {
-        return 0;
+        return numItems;
     }
 
     public boolean isThere(String item) {
-        return true;
+        boolean moreToSearch;
+        int location = 0;
+        boolean found = false;
+
+        moreToSearch = (location < numItems);
+
+        while (moreToSearch && !found) {
+            if (item.compareTo(list[location]) == 0)
+                found = true;
+            else {
+                location++;
+                moreToSearch = (location < numItems);
+            }
+        }
+
+        return found;
     }
 
     public void insert(String item) {
-
+        list[numItems] = new String((item));
+        numItems++;
     }
 
     public void delete(String item) {
+        int location = 0;
 
+        while (item.compareTo(list[location]) != 0)
+            location++;
+
+        list[location] = list[numItems - 1];
+        numItems--;
     }
 
     public void reset() {
-
+        currentPos = 0;
     }
 
     public String getNextItem() {
-        return new String();
+        String next = list[currentPos];
+
+        if(currentPos == numItems - 1)
+            currentPos = 0;
+        else
+            currentPos++;
+
+        return new String(next);
     }
 }
