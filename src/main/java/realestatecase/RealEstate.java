2677b904195e58c15810fe31709db1b7a71f77cd
Enrique Coello <enrique.coello@gmail.com>
2021-04-05 12:21:15 -0400

    Figured out how to read files

--- src/main/java/realestatecase/RealEstate.java
+++ src/main/java/realestatecase/RealEstate.java
@@ -1,7 +1,19 @@
 package realestatecase;
 
-public class RealEstate {
-    public static void main(String[] args) {
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class RealEstate {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src\\main\\resources\\houses.txt"));
+
+        System.out.println("lines: " + lines.size());
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
