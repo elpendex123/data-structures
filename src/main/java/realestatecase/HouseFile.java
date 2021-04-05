68a2f13f1584fc5f974443522baf03ce12ea4baa
Enrique Coello <enrique.coello@gmail.com>
2021-04-05 14:03:30 -0400

    Updated house file and house list

--- src/main/java/realestatecase/HouseFile.java
+++ src/main/java/realestatecase/HouseFile.java
@@ -1,4 +1,72 @@
 package realestatecase;
 
+import java.io.*;
+
 public class HouseFile {
+    private static BufferedReader inFile;
+    private static PrintWriter outFile;
+    private static boolean inFileOpen = false;
+    private static boolean outFileOpen = false;
+    private static String inString = "";
+
+    public static void reset() throws IOException {
+        if (inFileOpen) inFile.close();
+        if (outFileOpen) outFile.close();
+        inFile = new BufferedReader(new FileReader("src\\main\\resources\\houses.txt"));
+        inFileOpen = true;
+        inString = inFile.readLine();
+    }
+
+    public static void rewrite() throws IOException {
+        if (inFileOpen) inFile.close();
+        if (outFileOpen) outFile.close();
+        outFile = new PrintWriter(new FileWriter("src\\main\\resources\\houses.txt"));
+        outFileOpen = true;
+    }
+
+    public static boolean moreHouses()
+    {
+        if (!inFileOpen || (inString == null))
+            return false;
+        else
+            return true;
+    }
+
+    public static ListHouse getNextHouse() throws IOException {
+        int lotNumber = 0;
+        String lastName = "xxxxx";
+        String firstName = "xxxxx";
+        int price = 0;
+        int squareFeet = 0;
+        int bedRooms = 0;
+
+        lotNumber = Integer.parseInt(inFile.readLine());
+        lastName = inString;
+        firstName = inFile.readLine();
+        price = Integer.parseInt(inFile.readLine());
+        squareFeet = Integer.parseInt(inFile.readLine());
+        bedRooms = Integer.parseInt(inFile.readLine());
+
+        inString = inFile.readLine();
+
+        ListHouse house = new ListHouse(lotNumber, firstName, lastName, price, squareFeet, bedRooms);
+        return house;
+    }
+
+    public static void putToFile(ListHouse house)
+    {
+        outFile.println(house.getLotNumber());
+        outFile.println(house.getFirstName());
+        outFile.println(house.getLastName());
+        outFile.println(house.getPrice());
+        outFile.println(house.getSquareFeet());
+        outFile.println(house.getBedRooms());
+    }
+
+    public static void close() throws IOException {
+        if (inFileOpen) inFile.close();
+        if (outFileOpen) outFile.close();
+        inFileOpen = false;
+        outFileOpen = false;
+    }
 }
--- src/main/java/realestatecase/ListHouse.java
+++ src/main/java/realestatecase/ListHouse.java
@@ -3,13 +3,79 @@ package realestatecase;
 import genericLists.Listable;
 
 public class ListHouse implements Listable {
-    @Override
-    public int compareTo(Listable other) {
-        return 0;
+    private int lotNumber;
+    private String firstName;
+    private String lastName;
+    private int price;
+    private int squareFeet;
+    private int bedRooms;
+
+    public ListHouse(int lotNumber, String firstName, String lastName, int price, int squareFeet, int bedRooms) {
+        this.lotNumber = lotNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.price = price;
+        this.squareFeet = squareFeet;
+        this.bedRooms = bedRooms;
     }
 
     @Override
     public Listable copy() {
-        return null;
+        ListHouse result = new ListHouse(lotNumber, firstName, lastName, price, squareFeet, bedRooms);
+        return result;
+    }
+
+    @Override
+    public int compareTo(Listable otherListHouse) {
+        ListHouse other = (ListHouse) otherListHouse;
+        return (this.lotNumber - other.lotNumber);
+    }
+
+    public int getLotNumber() {
+        return lotNumber;
+    }
+
+    public void setLotNumber(int lotNumber) {
+        this.lotNumber = lotNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSquareFeet() {
+        return squareFeet;
+    }
+
+    public void setSquareFeet(int squareFeet) {
+        this.squareFeet = squareFeet;
+    }
+
+    public int getBedRooms() {
+        return bedRooms;
+    }
+
+    public void setBedRooms(int bedRooms) {
+        this.bedRooms = bedRooms;
     }
 }
